cbf34b1dc383becc9a1362cda113c779ffcd52fd
pawan bana <bana.pawan@gmail.com>
2018-12-08 13:21:34 +0530

    filtering

--- db/development.js
+++ db/development.js
@@ -2,7 +2,7 @@ var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 
 // Insert your development dataBase url in here .
-mongoose.connect('mongodb://writer:123abc@ds123844.mlab.com:23844/twitter-api',{useNewUrlParser:true});
+mongoose.connect('mongodb://writer:123abc@ds123844.mlab.com:23844/twitter-api',{ useCreateIndex: true,useNewUrlParser:true});
 
 module.exports={
 	mongoose
--- db/test.js
+++ db/test.js
@@ -3,7 +3,7 @@ mongoose.Promise=global.Promise;
 
 // Insert your Test dataBase url in here.
 
-mongoose.connect('mongodb://tester:123abc@ds127634.mlab.com:27634/twitter-api-test',{useNewUrlParser:true});
+mongoose.connect('mongodb://tester:123abc@ds127634.mlab.com:27634/twitter-api-test',{ useCreateIndex: true,useNewUrlParser:true});
 
 module.exports={
 	mongoose
